c0c0838cff8543c1e1cd0868baf7a952e378c41a
C0DEFpS <122454759+C0DEFpS@users.noreply.github.com>
2023-01-13 10:48:11 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,20 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(100)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(pontuacao)
+    basic.showString("de")
+    basic.showNumber(nrjogadas)
+    basic.pause(2000)
+    basic.clearScreen()
+    nrjogadas = 0
+    pontuacao = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(num1 * num2)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.B, function () {
     pontuacao += 1
     basic.showString("boa")
@@ -15,9 +29,11 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(100)
     basic.clearScreen()
 })
+let pontuacao = 0
+let nrjogadas = 0
 let num2 = 0
 let num1 = 0
 num1 = 0
 num2 = 0
-let nrjogadas = 0
-let pontuacao = 0
+nrjogadas = 0
+pontuacao = 0
